b46e1c946c88dbef80b038e028160b234141a8a4
Abishek R <abishek.r@srinsofttech.lan>
2023-08-14 17:26:21 +0530

    created app

---
+++ Courseappp/src/main/java/com/springboot/course/Courseappp/CourseapppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.course.Courseappp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseapppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseapppApplication.class, args);
+	}
+
+}
---
+++ Courseappp/src/test/java/com/springboot/course/Courseappp/CourseapppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.course.Courseappp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseapppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
